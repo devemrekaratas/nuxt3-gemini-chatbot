bd95981fe237196fbdaa890a12b1b73920445a2c
Emre Karatas <dev.emrekaratas@gmail.com>
2024-09-17 14:26:21 +0300

    Update Chatlist.vue

--- components/Sidebar/ChatList.vue
+++ components/Sidebar/ChatList.vue
@@ -4,6 +4,11 @@ const store = useChatStore()
 
 <template>
     <div class="flex flex-col gap-y-2 flex-1">
+        <div class="flex items-center gap-x-2 text-medium ms-2">
+            <UIcon name="i-material-symbols:history-rounded" class="w-5 h-5" />
+            Chat history
+        </div>
+        <UDivider />
         <div v-for="chat in store.chatList" :key="chat.id"
             :class="[{ 'bg-blue-500 dark:bg-blue-900 text-white': store.activeChat?.id === chat.id }]"
             class="flex items-center justify-between rounded-md">
